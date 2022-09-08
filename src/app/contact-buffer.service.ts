b1ca9aa149f116394288bc88486688891bdaa7f7
Pierre Bretéché <pbreteche@dawan.fr>
2022-09-08 11:25:47 +0200

    Gestion d'erreur HTTP

--- src/app/contact-buffer.service.ts
+++ src/app/contact-buffer.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Observable, ReplaySubject } from 'rxjs';
+import { catchError, Observable, of, ReplaySubject } from 'rxjs';
 import { ContactLoaderService } from './contact-loader.service';
 import { Contact } from './model/contact';
+import data from './stub/contacts.data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactBufferService {
   private subject = new ReplaySubject<Contact[]>(1)
-  private contacts: Contact[] = []  
+  private contacts: Contact[] = []
 
   constructor(private loader: ContactLoaderService) {
-    this.loader.get().subscribe((data: Contact[]) => {
+    this.loader.get()
+    .pipe(catchError(() => of(data)))
+    .subscribe((data: Contact[]) => {
       this.contacts = data
       this.subject.next(this.contacts)
     })
--- src/app/contact-loader.service.ts
+++ src/app/contact-loader.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, throwError } from 'rxjs';
 import { Contact } from './model/contact';
 
 @Injectable({
@@ -15,7 +15,16 @@ export class ContactLoaderService {
       .pipe(
         map(data => {
           return data as Contact[]
-        })
+        }),
+        catchError(
+          (err: HttpErrorResponse) => {
+            if (404 == err.status) {
+              return throwError(() => new Error('La ressource n\'a pas été trouvée'))
+            }
+
+            return throwError(() => new Error('Une erreur s\'est produite'))
+          }
+        )
       )
   }
 }
